cda9df9d79c071bf4021b46cb125c5d22692014f
Gustavo Camalionti <gustavocamalionti@gmail.com>
2026-04-10 15:46:35 -0300

    feat: remove unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import { useState } from "react";
-
-function App() {
-  const [count, setCount] = useState(0);
+export function App() {
   return (
     <>
       <p>Hello World</p>
-
-      <button onClick={() => setCount(count + 1)}>Count {count}</button>
     </>
   );
 }
-
-export default App;
