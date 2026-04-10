213f82c9cc3886b945058e79488633bd049b76e0
Gustavo Camalionti <gustavocamalionti@gmail.com>
2026-04-10 16:32:31 -0300

    feat: example basic using props

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
+import type React from "react";
+
+interface ICardProps {
+  title: string;
+  children: React.ReactNode;
+}
+const Card = (props: ICardProps) => {
+  return (
+    <div
+      style={{
+        border: "1px solid black",
+      }}
+    >
+      <span>Title: {props.title}</span>
+
+      <div>{props.children}</div>
+
+      <div>Footer</div>
+    </div>
+  );
+};
+
 export function App() {
   return (
-    <>
-      <p>Hello World</p>
-    </>
+    <div>
+      Hello World
+      <Card title="Teste">Teste</Card>
+      <Card title="Teste">Teste 1</Card>
+      <Card title="Teste">Teste 2</Card>
+      <Card title="Teste">Teste 3</Card>
+      <Card title="Teste">Teste 4</Card>
+      <Card title="Teste">Teste 5</Card>
+    </div>
   );
 }
