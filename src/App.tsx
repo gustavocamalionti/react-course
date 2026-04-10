6c97ee7ae64d8217ba986d1c57b2ee25c61c908c
Gustavo Camalionti <gustavocamalionti@gmail.com>
2026-04-10 17:17:03 -0300

    feat: implement list in react component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import { useState } from "react";
 
 export function App() {
-  const [count, setCount] = useState(0);
-  const [hide, setHide] = useState(false);
-
+  const [list, setList] = useState([
+    { id: "1", label: "Fazer café" },
+    { id: "2", label: "Fazer café" },
+    { id: "3", label: "Fazer almoço" },
+    { id: "4", label: "Fazer janta" },
+  ]);
   return (
     <div>
-      {hide && <p>Teste 1</p>}
-      {!hide && <p>Teste 2</p>}
-      {hide ? <p>Teste 1</p> : <p>Teste 2</p>}
-      <button onClick={() => setCount(count + 1)}>{count}</button>
-      <button onClick={() => setHide(!hide)}>Toggle</button>
+      <input />
+      <button>Adicionar</button>
+      <ol>
+        {list.map((listItem) => (
+          <li key={listItem.id}>{listItem.label}</li>
+        ))}
+      </ol>
     </div>
   );
 }
