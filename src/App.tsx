d87e1a026f44ea50d456fe5694e5f0ebe83b96df
Gustavo Camalionti <gustavocamalionti@gmail.com>
2026-04-17 16:51:41 -0300

    feat: cretea component `InputAdd`

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,49 @@
-import { useState } from "react";
+import { useState } from 'react';
+import { InputAdd } from './components/InputAdd';
 
 export function App() {
   const [list, setList] = useState([
-    { id: "1", label: "Fazer café" },
-    { id: "2", label: "Fazer café" },
-    { id: "3", label: "Fazer almoço" },
-    { id: "4", label: "Fazer janta" },
+    { id: '1', label: 'Fazer café', complete: false },
+    { id: '2', label: 'Fazer café', complete: false },
+    { id: '3', label: 'Fazer almoço', complete: false },
+    { id: '4', label: 'Fazer janta', complete: false },
   ]);
   return (
     <div>
-      <input />
-      <button>Adicionar</button>
+      <InputAdd
+        onAdd={(value) =>
+          setList([
+            ...list,
+            {
+              id: (list.length + 1).toString(),
+              complete: false,
+              label: value,
+            },
+          ])
+        }
+      />
+
       <ol>
         {list.map((listItem) => (
-          <li key={listItem.id}>{listItem.label}</li>
+          <li key={listItem.id}>
+            {listItem.label}
+            {listItem.complete ? 'Concluído' : ''}
+            <button
+              onClick={() =>
+                setList([
+                  ...list.map((item) => ({
+                    ...item,
+                    complete: item.id === listItem.id ? true : item.complete,
+                  })),
+                ])
+              }
+            >
+              Concluir
+            </button>
+            <button onClick={() => setList([...list.filter((item) => item.id !== listItem.id)])}>
+              Remover
+            </button>
+          </li>
         ))}
       </ol>
     </div>
