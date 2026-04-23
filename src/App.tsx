548d8c464f3a7190305021831ef2a14a960c597c
Gustavo Camalionti <gustavocamalionti@gmail.com>
2026-04-23 10:24:05 -0300

    style: move importations styles in  to `App.tsx`

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { Home } from './pages/Home';
 import { AppLayout } from './shared/layout/AppLayout';
+import './shared/styles/variables.css';
+import './shared/styles/base.css';
+import './shared/styles/components.css';
 
 export function App() {
   return (
