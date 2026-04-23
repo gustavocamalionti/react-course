f0028a2a551475d8def74858d9e9e3c71222ee3c
Gustavo Camalionti <gustavocamalionti@gmail.com>
2026-04-23 10:56:25 -0300

    feat: create About Page

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { AppLayout } from './shared/layout/AppLayout';
 import './shared/styles/variables.css';
 import './shared/styles/base.css';
 import './shared/styles/components.css';
+import { About } from './pages/About';
 
 export function App() {
   return (
     <AppLayout>
       <Home />
+      <About />
     </AppLayout>
   );
 }
---
+++ src/pages/About.tsx
@@ -0,0 +1,23 @@
+import { PageLayout } from '../shared/layout/page-layout/PageLayout';
+
+export const About = () => {
+  return (
+    <PageLayout title="Sobre">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat hic, consequuntur adipisci
+        ad cupiditate veritatis reprehenderit commodi ullam sit! Facere tempora nihil, aspernatur
+        accusamus deserunt quas earum! Totam, nihil asperiores.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat hic, consequuntur adipisci
+        ad cupiditate veritatis reprehenderit commodi ullam sit! Facere tempora nihil, aspernatur
+        accusamus deserunt quas earum! Totam, nihil asperiores.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat hic, consequuntur adipisci
+        ad cupiditate veritatis reprehenderit commodi ullam sit! Facere tempora nihil, aspernatur
+        accusamus deserunt quas earum! Totam, nihil asperiores.
+      </p>
+    </PageLayout>
+  );
+};
