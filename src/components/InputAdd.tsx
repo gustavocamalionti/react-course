b954d28ab5509915dff0b6ff37e08e1bef4f9229
Gustavo Camalionti <gustavocamalionti@gmail.com>
2026-04-23 15:47:02 -0300

    feat: add useRef for save inputElement focus

--- src/components/InputAdd.tsx
+++ src/components/InputAdd.tsx
@@ -1,21 +1,24 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import StylesInputAdd from './InputAdd.module.css';
 interface IInputAddProps {
   onAdd(value: string): void;
 }
 
 export const InputAdd = (props: IInputAddProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
   const [value, setValue] = useState('');
 
   const handleAdd = () => {
     props.onAdd(value);
     setValue('');
+    inputRef.current?.focus();
   };
 
   return (
     <div className={StylesInputAdd.ComponentContainer}>
       <input
         value={value}
+        ref={inputRef}
         onChange={(e) => setValue(e.target.value)}
         className={StylesInputAdd.ComponentInput}
       />
